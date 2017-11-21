cd11e1f48a3a008b8594eba985a0cfe1e39a06f0
João Paulo <jpdik@hotmail.com>
2017-11-21 15:11:03 -0200

    Correção Init
    Correção Init

--- EJB/SuperDiaClient/ejbModule/br/com/interfacebean/IAutentica.java
+++
@@ -1,7 +0,0 @@
-package br.com.interfacebean;
-
-import br.com.modelo.Usuario;
-
-public interface IAutentica {
-	Usuario autentica(Usuario usuario);
-}
--- EJB/SuperDiaClient/ejbModule/br/com/interfacebean/ICliente.java
+++
@@ -1,9 +0,0 @@
-package br.com.interfacebean;
-
-import br.com.modelo.Usuario;
-
-public interface ICliente {
-	void autoAdiciona(Usuario usuario);
-	void autoAltera(Usuario usuario);
-	void autoRemove(Usuario usuario);
-}
