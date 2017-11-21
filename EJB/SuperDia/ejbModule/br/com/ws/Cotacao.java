1b5378ecc42c2eedd7f9ce8e0f54806579967ef6
João Paulo <jpdik@hotmail.com>
2017-11-21 14:24:36 -0200

    WS
    WS

---
+++ EJB/SuperDia/ejbModule/br/com/ws/Cotacao.java
@@ -0,0 +1,25 @@
+package br.com.ws;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.jws.WebService;
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@WebService
+@Stateless
+@LocalBean
+public class Cotacao {
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String DollarToReal() {
+		return "R$ 3.40";
+	}
+	
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getEuroToReal() {
+		return "R$ 4.10";
+	}
+}
