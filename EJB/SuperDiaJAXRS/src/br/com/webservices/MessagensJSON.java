4d42cb6db02ebf119ebd3790f20929fc9c33b962
Deborah <deborahferreirafonseca@gmail.com>
2017-12-08 08:53:54 -0200

    Adicionando mensagens de retorno na API para método cadastrar.

--- EJB/SuperDiaJAXRS/src/br/com/webservices/MessagensJSON.java
+++ EJB/SuperDiaJAXRS/src/br/com/webservices/MessagensJSON.java
@@ -1,8 +1,14 @@
 package br.com.webservices;
 
+/**
+ * @author Deborah
+ */
 public enum MessagensJSON {
 	//JSON Usuário
-	ERRO_USUARIO_INVALIDO("{'cod' : 401, 'msg':'Usuário não tem permissão para realizar esta operação.'}");
+	ERRO_USUARIO_INVALIDO("{'cod' : 401, 'msg':'Usuário não tem permissão para realizar esta operação.'}"),
+	USUARIO_CADASTRADO_SUCESSO("{'cod': 200, 'msg': 'Cadastro realizado com sucesso!'}"),
+	USUARIO_CADASTRADO_FALHA("{'cod': 401, 'msg': 'Não foi possível cadastrar usuário.'}");
+	
 	
 	private MessagensJSON(String mensagem) {
 		this.mensagem = mensagem;
