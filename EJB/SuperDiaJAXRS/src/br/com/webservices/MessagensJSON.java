7b205c7cdecf12ecca6a884dcc1f07395628ea4d
Deborah <deborahferreirafonseca@gmail.com>
2017-12-08 08:42:24 -0200

    Enumeração com os possíveis erros da API

--- EJB/SuperDiaJAXRS/src/br/com/webservices/MessagensJSON.java
+++ EJB/SuperDiaJAXRS/src/br/com/webservices/MessagensJSON.java
@@ -1,6 +1,18 @@
 package br.com.webservices;
 
-public class MessagensJSON {
+public enum MessagensJSON {
+	//JSON Usuário
+	ERRO_USUARIO_INVALIDO("{'cod' : 401, 'msg':'Usuário não tem permissão para realizar esta operação.'}");
+	
+	private MessagensJSON(String mensagem) {
+		this.mensagem = mensagem;
+	}
 
+	private String mensagem;
+
+	public String getMensagem() {
+		return mensagem;
+}
+	
 	
 }
