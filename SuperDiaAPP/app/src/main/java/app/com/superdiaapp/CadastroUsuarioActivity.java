005f29b93d8b472d7b4af575ff06335f7b878dd2
Érika <deborahferreirafonseca@gmail.com>
2017-12-14 15:47:36 -0200

    Add projeto android

---
+++ SuperDiaAPP/app/src/main/java/app/com/superdiaapp/CadastroUsuarioActivity.java
@@ -0,0 +1,13 @@
+package app.com.superdiaapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CadastroUsuarioActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_usuario);
+    }
+}
---
+++ SuperDiaAPP/app/src/main/java/app/com/superdiaapp/SobreActivity.java
@@ -0,0 +1,13 @@
+package app.com.superdiaapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SobreActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sobre);
+    }
+}
