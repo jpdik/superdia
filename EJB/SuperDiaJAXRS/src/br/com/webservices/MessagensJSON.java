2301011041b81a4adddc8e37d3cdde9a11432e38
Deborah <deborahferreirafonseca@gmail.com>
2017-12-14 09:21:12 -0200

    Mudando o código de resposta da MensagensJSON

--- EJB/SuperDiaJAXRS/src/br/com/webservices/MessagensJSON.java
+++ EJB/SuperDiaJAXRS/src/br/com/webservices/MessagensJSON.java
@@ -5,15 +5,15 @@ package br.com.webservices;
  */
 public enum MessagensJSON {
 	//JSON Usuário
-	ERRO_USUARIO_INVALIDO("{'cod' : 401, 'msg':'Usuário não tem permissão para realizar esta operação.', 'usuario':{%s}}"),
+	ERRO_USUARIO_INVALIDO("{'cod' : 200, 'msg':'Usuário não tem permissão para realizar esta operação.', 'usuario':{%s}}"),
 	USUARIO_CADASTRADO_SUCESSO("{'cod': 200, 'msg': 'Cadastro realizado com sucesso!'}"),
-	USUARIO_CADASTRADO_FALHA("{'cod': 401, 'msg': 'Falha no Cadastro(usuário já existente).'}"),
+	USUARIO_CADASTRADO_FALHA("{'cod': 200, 'msg': 'Falha no Cadastro(usuário já existente).'}"),
 	ALTERAR_USUARIO_SUCESSO("{'cod' : 200, 'msg':'Informações alteradas com sucesso!'}"), 
-	USUARIO_INALTERADO("{'cod' : 401, 'msg':'Usuário já existente. (Inalterado)'}"),
-	ALTERAR_USUARIO_FALHA("{'cod' : 401, 'msg':'Usuário que deseja alterar não existe, ou informações não coincidem.'}"),
+	USUARIO_INALTERADO("{'cod' : 200, 'msg':'Usuário já existente. (Inalterado)'}"),
+	ALTERAR_USUARIO_FALHA("{'cod' : 200, 'msg':'Usuário que deseja alterar não existe, ou informações não coincidem.'}"),
 	INFORMACAO_ALTERADA_SUCESSO("{'cod' : 200, 'msg':'Informações alteradas com sucesso!'}"), 
 	USUARIO_REMOVIDO_SUCESSO("{'cod' : 200, 'msg':'Usuário removido com sucesso!'}"), 
-	USUARIO_NAO_EXISTE("{'cod' : 401, 'msg':'Usuário removido com sucesso!'}"),
+	USUARIO_NAO_EXISTE("{'cod' : 200, 'msg':'Usuário removido com sucesso!'}"),
 	RETORNA_USUARIO("{'cod': 200, 'msg': 'Login realizado com sucesso!', 'usuario':{%s}}");
 	
 	
